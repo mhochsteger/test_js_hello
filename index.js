9f45b36339b6c7e96d6392230eaf61ff13814ce3
Matthias Hochsteger <mhochsteger@cerbsim.com>
2020-05-11 13:22:16 +0200

    update

--- index.js
+++ index.js
@@ -1,46 +1,27 @@
-(function(window) {
-
-	/* This example module depends on jQuery and lodash */
-	var factory = function(widgets) {
-		'use strict';
-
-		var test_js_hello = function(opts) {
-			this.options = $.extend({}, test_js_hello.defaultOptions, opts);
-			this.init();
-		};
-
-		test_js_hello.defaultOptions = {
-			// default module options go here
-		};
-
-		test_js_hello.prototype.init = function() {
-                  console.log("init test_js_hello module!");
-		};
-
-		// extra module functions go here
-
-		return test_js_hello;
+// checking environment, if it is not AMD, emulate define for Node
+if (typeof define !== 'function' || ! define.amd)
+	var define = function(moduleName, requirements, func) {
+		var dependencies = [];
+		requirements.forEach(function(req) { dependencies.push( require(req) ); });
+		func.apply(this, dependencies);
 	};
 
-	// Define as an AMD module if possible
-	if (typeof define === 'function' && define.amd) {
-		define(['@jupyter-widgets/base'], factory);
-	}
-	// Node/CommonJS
-	else if (typeof exports === 'object') {
-		factory(require('@jupyter-widgets/base'));
-	}
-	// Otherwise simply initialise as normal, stopping multiple evaluation
-	else {
-		// check that the module dependencies are defined
-		if (typeof widgets === 'undefined') {
-			throw new Error('test_js_hello requires @jupyter-widgets/base');
-		}
 
-		// define module only if it doesn't exist yet
-		if (typeof test_js_hello === 'undefined') {
-			window.test_js_hello = factory(widgets);
-		}
-	}
+define('test-js-hello' /* or 'app/test-js-hello' - see comment above */,
+	['@jupyter-widgets/base' /* , more required dependencies */ ],
+function(widgets /*, dependencies variables */)
+{
+        console.log("widgets", widgets);
+	var test-js-hello = {};
 
-})(window);
+	// exports for Node
+	if (typeof module !== 'undefined' && module.exports)
+		module.exports = test-js-hello;
+
+	//
+	// module code
+	//
+
+	// exports for AMD (requirejs)
+	return test-js-hello;
+});
